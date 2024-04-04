b4fe0e7d1bd72191dffe24baee29500ab3564534
Nicolai Wisselink <nicolai.wisselink@hva.nl>
2024-04-04 21:12:53 +0200

    l

--- src/api/src/julian/items/NotebookItem.ts
+++ src/api/src/julian/items/NotebookItem.ts
@@ -45,24 +45,18 @@ export class NotebookItem extends Item implements Examine, Pickup{
         }
         if (playerSession.searchedStars || playerSession.starmapInspected) {
             hints.push("<blue>Submarine clues:</blue>");
-<<<<<<< HEAD
-            hints.push("Since you can almost count the amount of pixels of the <blue>Starmap</blue>... <blue>It's October</blue>.");
-        }
 
-        if (!playerSession.oasisPuzzleHints.includes(1) && !playerSession.logPuzzleTried) {
-            return new TextActionResult(["You haven't written anything down in your notebook yet.", "Gather clues and <blue>Examine your Notebook</blue> to see your hints."]);
-=======
         }
         if (playerSession.searchedStars) {
-            hints.push("According to Noodle, the stars gradually shift towards the right every month");
+            hints.push("According to Noodle, the stars gradually shift towards the right every month.");
         }
         if (playerSession.starmapInspected) {
-            hints.push("I should use the <blue>computer</blue> to do some research about <blue>starmaps</blue>");
+            hints.push("I should use the <blue>Computer</blue> to do some research about <blue>Starmaps</blue>.");
         }
 
         if (!playerSession.oasisPuzzleHints.includes(1) && !playerSession.searchedStars && !playerSession.starmapInspected) {
-            return new TextActionResult(["You haven't written anything down in your notebook yet", "Gather clues and <blue>Examine your Notebook</blue> to see your hints"]);
->>>>>>> 6b5b34b0b55760f60306a8674f0a2d0e2b19f0c2
+            return new TextActionResult(["You haven't written anything down in your <blue>Notebook<blue> yet.", "Gather clues and <blue>Examine your Notebook</blue> to see your hints"]);
+
         } else {
             return new TextActionResult(hints);
         }
