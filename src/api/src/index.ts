3e7135864d6f5512fe8d4df825aba7b3907d49a0
Julian van der Linde <lindejc@hva.nl>
2026-06-06 00:08:26 +0200

    fix5

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -2,12 +2,21 @@ import express, { Express } from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import { router } from "./routes";
+
 export const app: Express = express();
 
 config();
 config({ path: ".env.local", override: true });
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://julian-1301.github.io",
+        "https://julian-1301.github.io",
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/", router);
